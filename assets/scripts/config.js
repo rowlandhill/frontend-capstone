643f5a6f60f033e1408f1d2ebe8b2e0fb114216e
rowlandhill <hill.rowland@gmail.com>
2017-06-15 23:15:36 -0400

    Adds Heroku URL to config.js
    Heroku URL added to project.  Will merge with master, then push to git

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 const config = {
   apiOrigins: {
-    production: 'https://ga-wdi-boston.herokuapp.com',
+    production: 'https://infinite-lake-73977.herokuapp.com/',
     development: 'http://localhost:4741'
   }
 }
